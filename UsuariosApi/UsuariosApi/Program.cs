488bf05f29849789628515a4d48a3387ac1ce01b
João Marcelo Batista Narciso <joaomarcelobatistanarciso@gmail.com>
2023-07-09 11:18:16 -0300

    Adicionando o método de Login no controller usando o UsuarioService

--- UsuariosApi/UsuariosApi/Program.cs
+++ UsuariosApi/UsuariosApi/Program.cs
@@ -26,7 +26,7 @@ builder.Services.AddAutoMapper
     (AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services
-    .AddScoped<CadastroService>();
+    .AddScoped<UsuarioService>(); 
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
